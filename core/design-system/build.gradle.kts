bcdf9680fb4aab944b449790f5833c4fd90300b9
유섭 <yoonbk2001027@gmail.com>
2026-02-08 23:19:04 +0900

    refactor :: buildSrc에 적용된 의존성 제거

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -12,12 +12,3 @@ apply<ComposeGradlePlugin>()
 android {
     namespace = "com.yuseob.android.core.design_system"
 }
-
-dependencies {
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-
-    debugImplementation(libs.androidx.compose.ui.tooling)
-}
