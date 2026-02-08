c2ea5b9bcabc6ca548e36adfe6f4cd7ed7e6efbc
유섭 <yoonbk2001027@gmail.com>
2026-02-08 18:45:33 +0900

    chore :: .claude 경로의 파일들 ignore

--- buildSrc/src/main/java/CommonGradlePlugin.kt
+++ buildSrc/src/main/java/CommonGradlePlugin.kt
@@ -2,7 +2,6 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.project
 
 class CommonGradlePlugin : Plugin<Project> {
     override fun apply(project: Project) {
