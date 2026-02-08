67e0c270bece81cd29abab295de4d53fe8fa4f78
유섭 <yoonbk2001027@gmail.com>
2026-02-08 20:50:25 +0900

    build :: complie sdk, 라이브러리 버전 업

--- buildSrc/src/main/java/ProjectProperties.kt
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -1,7 +1,7 @@
 object ProjectProperties {
-    const val COMPILE_SDK = 34
+    const val COMPILE_SDK = 36
     const val MIN_SDK = 26
-    const val TARGET_SDK = 34
+    const val TARGET_SDK = 36
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
     const val COMPOSE_VERSION = "1.5.6"
